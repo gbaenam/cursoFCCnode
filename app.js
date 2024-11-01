34150ae9d1338f417bb2ce9c488dc346cb7d3708
German Baena <germanb66@gmail.com>
2024-11-01 12:30:32 -0500

    Se importó "cursos.js", se creó el servidor con node y se está implementando "Routng"

--- app.js
+++ app.js
@@ -2,4 +2,30 @@
 const http = require('http')
 const cursos = require('./cursos.js')
 
-console.log(cursos)
+servidor = http.createServer((req, res) => {
+    const { method } = req
+
+    switch(method) {
+        case 'GET':
+            return manejarSolicitudGET(req, res)
+        default:
+            console.log(`El método ${method}, no puede ser manejado por el Servidor`)
+    }
+
+})
+
+function manejarSolicitudGET(req, res) {
+    const path = req.url
+
+    if (path === '/') {
+        res.statusCode = 200
+        res.end('Bienvenidos a mi primer servidor y API creados con Node.js')
+    }
+}
+
+const PUERTO = 3000
+
+servidor.listen(PUERTO, () => {
+    console.log(`Servidor escuchando en el ${PUERTO} `)
+})
+
